06d0e8dfdc13cc71e9a24ca7263c6e48ee1c8279
Anton Jerey <antonjerey@gmail.com>
2020-03-08 10:55:06 +0100

    Y Scale now adapts to given data. (#15)

--- webserver/public/js/chartHelper.js
+++ webserver/public/js/chartHelper.js
@@ -7,7 +7,7 @@ $(function () {
       allTempTimeStamps.push(element.timestamp)
       allTemperatures.push(element.temperature)
     })
-    updateData(TemperatureChart, allTemperatures, allTempTimeStamps, chartColors.red, 0, 40)
+    updateData(TemperatureChart, allTemperatures, allTempTimeStamps, chartColors.red, 1)
   })
   socket.on('setLastHumDataByMac', function (data) {
     allHumTimeStamps = []
@@ -16,7 +16,7 @@ $(function () {
       allHumTimeStamps.push(element.timestamp)
       allHumidities.push(element.humidity)
     })
-    updateData(HumidityChart, allHumidities, allHumTimeStamps, chartColors.blue, 0, 100)
+    updateData(HumidityChart, allHumidities, allHumTimeStamps, chartColors.blue, 5)
   })
   socket.on('setLastVoltDataByMac', function (data) {
     allTempTimeStamps = []
@@ -25,7 +25,7 @@ $(function () {
       allTempTimeStamps.push(element.timestamp)
       allVoltages.push(element.voltage)
     })
-    updateData(VoltageChart, allVoltages, allTempTimeStamps, chartColors.purple, 0, 4)
+    updateData(VoltageChart, allVoltages, allTempTimeStamps, chartColors.purple, 0.5)
   })
 })
 
@@ -41,15 +41,15 @@ var chartColors = {
 
 TemperatureChart = assignChartToCanvas('#TemperatureChart', {
   label: 'Temperatures'
-}, chartColors.red, 0, 40)
+}, chartColors.red, 1)
 HumidityChart = assignChartToCanvas('#HumidityChart', {
   label: 'Humidity'
-}, chartColors.blue, 0, 100)
+}, chartColors.blue, 5)
 VoltageChart = assignChartToCanvas('#VoltageChart', {
   label: 'Voltage'
-}, chartColors.purple, 0, 4)
+}, chartColors.purple, 0.5)
 
-function assignChartToCanvas(chartId, datas, color, min, max) {
+function assignChartToCanvas(chartId, datas, color, stepSize) {
   var ctxChart = $(chartId)
   return new Chart(ctxChart, {
     type: 'line',
@@ -65,8 +65,7 @@ function assignChartToCanvas(chartId, datas, color, min, max) {
       scales: {
         yAxes: [{
           ticks: {
-            max: max,
-            min: min
+            stepSize: stepSize
           }
         }]
       }
@@ -74,9 +73,9 @@ function assignChartToCanvas(chartId, datas, color, min, max) {
   })
 }
 function addData(chart, newData, timeStamp) {
-  var oldestTimeStampOfTheChart = Date.parse(chart.data.labels[0])/1000
-  var timeStamp24hpast = Math.round((new Date()).getTime() / 1000) - (24*3600)
-  if (oldestTimeStampOfTheChart < timeStamp24hpast) { 
+  var oldestTimeStampOfTheChart = Date.parse(chart.data.labels[0]) / 1000
+  var timeStamp24hpast = Math.round((new Date()).getTime() / 1000) - (24 * 3600)
+  if (oldestTimeStampOfTheChart < timeStamp24hpast) {
     chart.data.labels.splice(0, 1) // remove first label
     chart.data.datasets[0].data.splice(0, 1) // remove first data point
     chart.update()
@@ -85,14 +84,12 @@ function addData(chart, newData, timeStamp) {
   chart.data.datasets[0].data.push(newData) // add new data at end
   chart.update()
 }
-function updateData(chart, newData, newLabels, color, min, max) {
+function updateData(chart, newData, newLabels, color, stepSize) {
   chart.data.datasets[0].data = newData
   chart.data.labels = newLabels
   chart.data.datasets[0].borderColor = color
-  chart.options.scales.yAxes[0].ticks.min = min
-  chart.options.scales.yAxes[0].ticks.max = max
-  chart.options.scales.xAxes[0] = {
-    type: 'time'
-  }
+  chart.options.scales.yAxes[0].type = 'linear'
+  chart.options.scales.yAxes[0].ticks.stepSize = stepSize
+  chart.options.scales.xAxes[0].type = 'time'
   chart.update(0)
 }
